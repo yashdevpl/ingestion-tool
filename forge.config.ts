cca7cda3aa0e1d691df252723f70c3ee2ae2c283
yashdevpl <yash.doifode@pi-labs.ai>
2025-10-01 16:50:36 +0530

    feat : package building changes

--- forge.config.ts
+++ forge.config.ts
@@ -71,6 +71,19 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const config: ForgeConfig = {
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "yashdevpl",
+          name: "vox-app",
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
   packagerConfig: {
     protocols: [
       {
